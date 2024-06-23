9c4bdd32b3c0538f2c6c5b7920e4afd8a87105d2
LuanFabricio <luanlimaleite@hotmail.com>
2024-06-23 17:43:32 -0300

    feat: Adding body movement and increase body
    Also moving player speed from move_step based to player size.

--- znake/src/snake/snake.zig
+++ znake/src/snake/snake.zig
@@ -19,17 +19,17 @@ pub const MoveDirection = enum(usize) {
 pub const Snake = struct {
     head: Box,
     body: BoxArray,
-    move_step: f32,
+    size: f32,
     move_direction: MoveDirection,
 
-    pub fn init(allocator: Allocator, pos: ray.Vector2, move_step: f32, move_direction: MoveDirection) !Snake {
+    pub fn init(allocator: Allocator, pos: ray.Vector2, size: f32, move_direction: MoveDirection) !Snake {
         var snake: Snake = undefined;
         snake.head.pos = pos;
-        snake.head.size.x = 32;
-        snake.head.size.y = 32;
+        snake.head.size.x = size;
+        snake.head.size.y = size;
         snake.head.color = ray.GREEN;
 
-        snake.move_step = move_step;
+        snake.size = size;
         snake.move_direction = move_direction;
         snake.body = try BoxArray.init(allocator);
 
@@ -45,20 +45,58 @@ pub const Snake = struct {
     }
 
     pub fn move(self: *Snake) void {
+        const oldPosition = self.head;
+
+        const speed = self.calcSpeed();
         switch (self.move_direction) {
             MoveDirection.Up => {
-                self.head.pos.y -= self.move_step;
+                self.head.pos.y -= speed;
             },
             MoveDirection.Down => {
-                self.head.pos.y += self.move_step;
+                self.head.pos.y += speed;
             },
             MoveDirection.Left => {
-                self.head.pos.x -= self.move_step;
+                self.head.pos.x -= speed;
             },
             MoveDirection.Right => {
-                self.head.pos.x += self.move_step;
+                self.head.pos.x += speed;
             },
         }
+
+        const bodyLen = self.body.items.len;
+        var i = if (bodyLen > 0) self.body.items.len - 1 else 0;
+        while (i > 0) : (i -= 1) {
+            self.body.items[i] = self.body.items[i - 1];
+        }
+        if (bodyLen > 0) self.body.items[i] = oldPosition;
+    }
+
+    pub fn increaseBody(self: *Snake) !void {
+        var clone = self.head.clone();
+        const speed = self.calcSpeed();
+
+        switch (self.move_direction) {
+            MoveDirection.Up => {
+                clone.pos.y += speed;
+            },
+            MoveDirection.Down => {
+                clone.pos.y -= speed;
+            },
+            MoveDirection.Right => {
+                clone.pos.x += speed;
+            },
+            MoveDirection.Left => {
+                clone.pos.x -= speed;
+            },
+        }
+
+        try self.body.add(clone);
+    }
+
+    fn calcSpeed(self: *Snake) f32 {
+        const frameTime = ray.GetFrameTime();
+        const speed = self.size * frameTime * 4.5;
+        return speed;
     }
 
     pub fn updateDirection(self: *Snake) void {
